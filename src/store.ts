ea362d80d3ef4700ad21069a862092c8bd38eb0d
dplamenov <dimitar.plamenov@gmail.com>
2022-05-14 10:13:22 +0300

    store tickets

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,13 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit'
 import Ticket from './interfaces/Ticket';
+import { v4 as uuidv4 } from 'uuid';
 
 const ticketsSlice = createSlice({
   name: 'tickets',
   initialState: (JSON.parse(localStorage.getItem('tickets') as string) || []) as Ticket[],
   reducers: {
     create: (state, action) => {
-      state.push({ ...action.payload });
+      state.push({ ...action.payload, id: uuidv4() });
     }
   }
 });
