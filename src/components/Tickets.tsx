6afc3225a2f570bf51732c9732b89c197ead53b0
dplamenov <dimitar.plamenov@gmail.com>
2022-05-14 11:50:41 +0300

    show "No tickets." in tickets length is 0

--- src/components/Tickets.tsx
+++ src/components/Tickets.tsx
@@ -21,32 +21,35 @@ function Tickets() {
   }, []);
   
   return (
-    <Box sx={{ display: 'flex' }}>
-      <List sx={style} component="nav" aria-label="mailbox folders">
-        <h2>Todo</h2>
-        {tickets.filter(ticket => ticket.status === Status.ToDo.valueOf()).map((ticket, key) => {
-          return <TicketCard key={key} ticket={ticket}/>
-        })}
-      </List>
-      <List sx={style} component="nav" aria-label="mailbox folders">
-        <h2>In progress</h2>
-        {tickets.filter(ticket => ticket.status === Status.InProgress.valueOf()).map((ticket, key) => {
-          return <TicketCard key={key} ticket={ticket} />
-        })}
-      </List>
-      <List sx={style} component="nav" aria-label="mailbox folders">
-        <h2>In review</h2>
-        {tickets.filter(ticket => ticket.status === Status.InReview.valueOf()).map((ticket, key) => {
-          return <TicketCard key={key} ticket={ticket} />
-        })}
-      </List>
-      <List sx={style} component="nav" aria-label="mailbox folders">
-        <h2>Done</h2>
-        {tickets.filter(ticket => ticket.status === Status.Done.valueOf()).map((ticket, key) => {
-          return <TicketCard key={key} ticket={ticket} />
-        })}
-      </List>
-    </Box>
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <List sx={style} component="nav" aria-label="mailbox folders">
+          <h2>Todo</h2>
+          {tickets.filter(ticket => ticket.status === Status.ToDo.valueOf()).map((ticket, key) => {
+            return <TicketCard key={key} ticket={ticket} />
+          })}
+        </List>
+        <List sx={style} component="nav" aria-label="mailbox folders">
+          <h2>In progress</h2>
+          {tickets.filter(ticket => ticket.status === Status.InProgress.valueOf()).map((ticket, key) => {
+            return <TicketCard key={key} ticket={ticket} />
+          })}
+        </List>
+        <List sx={style} component="nav" aria-label="mailbox folders">
+          <h2>In review</h2>
+          {tickets.filter(ticket => ticket.status === Status.InReview.valueOf()).map((ticket, key) => {
+            return <TicketCard key={key} ticket={ticket} />
+          })}
+        </List>
+        <List sx={style} component="nav" aria-label="mailbox folders">
+          <h2>Done</h2>
+          {tickets.filter(ticket => ticket.status === Status.Done.valueOf()).map((ticket, key) => {
+            return <TicketCard key={key} ticket={ticket} />
+          })}
+        </List>
+      </Box>
+      {tickets.length === 0 && <p>No tickets.</p>}
+    </>
   )
 }
 
