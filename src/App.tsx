0724233c3d1aec8ef6c3b1d4655a96e5fe6059d7
dplamenov <dimitar.plamenov@gmail.com>
2022-05-14 09:01:24 +0300

    implement design for all tickets

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TicketCard from './components/TicketCard';
+import { Box, List } from '@mui/material';
+import Status from './interfaces/Status';
+
+const ticket = {
+  id: '1',
+  label: 'test',
+  estimationValue: '2 days',
+  assignedUser: {
+    username: 'dimitar'
+  },
+  status: Status.InReview
+};
+
+const style = {
+  width: '100%',
+  bgcolor: 'background.paper',
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{ display: 'flex' }}>
+        <List sx={style} component="nav" aria-label="mailbox folders">
+          <h2>Todo</h2>
+          <TicketCard ticket={ticket} />
+          <TicketCard ticket={ticket} />
+          <TicketCard ticket={ticket} />
+          <TicketCard ticket={ticket} />
+        </List>
+        <List sx={style} component="nav" aria-label="mailbox folders">
+          <h2>In progress</h2>
+          <TicketCard ticket={ticket} />
+          <TicketCard ticket={ticket} />
+          <TicketCard ticket={ticket} />
+        </List>
+        <List sx={style} component="nav" aria-label="mailbox folders">
+          <h2>In review</h2>
+          <TicketCard ticket={ticket} />
+        </List>
+        <List sx={style} component="nav" aria-label="mailbox folders">
+          <h2>Done</h2>
+          <TicketCard ticket={ticket} />
+          <TicketCard ticket={ticket} />
+        </List>
+      </Box>
     </div>
   );
 }
